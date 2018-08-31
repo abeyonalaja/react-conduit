b79b5f43e88165da6b65e23b9db2ac04851d267c
Abey Onalaja <abey.onalaja@gmail.com>
2018-08-31 16:05:37 +0100

    Creat a store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import { promiseMiddleware } from "./middleware";
 import "./index.css";
 import App from "./App";
+import store from "./store";
 import registerServiceWorker from "./registerServiceWorker";
-
-const defaultState = {
-  appName: "conduit",
-  articles: null
-};
-
-const reducer = function(state = defaultState, action) {
-  switch (action.type) {
-    case "HOME_PAGE_LOADED":
-      return { ...state, articles: action.payload.articles };
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
